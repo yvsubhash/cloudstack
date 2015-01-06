6fa1bd8ca63bcdc711bf0b4a1b2787d0a2dca7b9
Daan Hoogland <daan@onecht.net>
2015-01-06 17:39:44 +0100

    nic shouldn't inherit from net but be part of it/hooked into it

--- api/src/com/cloud/agent/api/to/NicTO.java
+++ api/src/com/cloud/agent/api/to/NicTO.java
@@ -16,9 +16,110 @@
 // under the License.
 package com.cloud.agent.api.to;
 
+import java.net.URI;
 import java.util.List;
 
-public class NicTO extends NetworkTO {
+import com.cloud.network.Networks.BroadcastDomainType;
+import com.cloud.network.Networks.TrafficType;
+
+public class NicTO {
+    NetworkTO net;
+    public BroadcastDomainType getBroadcastType() {
+        return net.getBroadcastType();
+    }
+
+    public void setBroadcastType(BroadcastDomainType broadcastType) {
+        net.setBroadcastType(broadcastType);
+    }
+
+    public void setIp(String ip) {
+        net.setIp(ip);
+    }
+
+    public void setNetmask(String netmask) {
+        net.setNetmask(netmask);
+    }
+
+    public void setGateway(String gateway) {
+        net.setGateway(gateway);
+    }
+
+    public void setMac(String mac) {
+        net.setMac(mac);
+    }
+
+    public void setDns1(String dns1) {
+        net.setDns1(dns1);
+    }
+
+    public void setDns2(String dns2) {
+        net.setDns2(dns2);
+    }
+
+    public void setType(TrafficType type) {
+        net.setType(type);
+    }
+
+    public void setName(String name) {
+        net.setName(name);
+    }
+
+    public String getName() {
+        return net.getName();
+    }
+
+    public void setSecurityGroupEnabled(boolean enabled) {
+        net.setSecurityGroupEnabled(enabled);
+    }
+
+    public String getIp() {
+        return net.getIp();
+    }
+
+    public String getNetmask() {
+        return net.getNetmask();
+    }
+
+    public String getGateway() {
+        return net.getGateway();
+    }
+
+    public String getMac() {
+        return net.getMac();
+    }
+
+    public String getDns1() {
+        return net.getDns1();
+    }
+
+    public String getDns2() {
+        return net.getDns2();
+    }
+
+    public TrafficType getType() {
+        return net.getType();
+    }
+
+    public URI getBroadcastUri() {
+        return net.getBroadcastUri();
+    }
+
+    public void setBroadcastUri(URI broadcastUri) {
+        net.setBroadcastUri(broadcastUri);
+    }
+
+    public URI getIsolationUri() {
+        return net.getIsolationUri();
+    }
+
+    public void setIsolationuri(URI isolationUri) {
+        net.setIsolationuri(isolationUri);
+    }
+
+    public boolean isSecurityGroupEnabled() {
+        return net.isSecurityGroupEnabled();
+    }
+
     int deviceId;
     Integer networkRateMbps;
     Integer networkRateMulticastMbps;
@@ -28,7 +129,7 @@ public class NicTO extends NetworkTO {
     List<String> nicSecIps;
 
     public NicTO() {
-        super();
+        net = new NetworkTO();
     }
 
     public void setDeviceId(int deviceId) {
@@ -67,19 +168,17 @@ public class NicTO extends NetworkTO {
         return pxeDisable;
     }
 
-    @Override
     public String getUuid() {
         return nicUuid;
     }
 
-    @Override
     public void setUuid(String uuid) {
         this.nicUuid = uuid;
     }
 
     @Override
     public String toString() {
-        return new StringBuilder("[Nic:").append(type).append("-").append(ip).append("-").append(broadcastUri).append("]").toString();
+        return new StringBuilder("[Nic:").append(net.type).append("-").append(net.ip).append("-").append(net.broadcastUri).append("]").toString();
     }
 
     public void setNicSecIps(List<String> secIps) {
@@ -91,10 +190,10 @@ public class NicTO extends NetworkTO {
     }
 
     public String getNetworkUuid() {
-        return super.getUuid();
+        return net.getUuid();
     }
 
     public void setNetworkUuid(String uuid) {
-        super.setUuid(uuid);
+        net.setUuid(uuid);
     }
 }
