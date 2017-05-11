2f0ae183058e518b739ef83820204793e608b584
Suresh Kumar Anaparti <suresh.anaparti@accelerite.com>
2017-05-12 02:41:24 +0530

    CLOUDSTACK-8612 [VMware] Make vCenter session timeout configurable for volume snapshot

--- setup/db/db/schema-4930to41000.sql
+++ setup/db/db/schema-4930to41000.sql
@@ -246,3 +246,5 @@ CREATE TABLE `cloud`.`guest_os_details` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `user_ip_address` ADD COLUMN `rule_state` VARCHAR(32) COMMENT 'static  rule state while removing';
+
+INSERT IGNORE INTO `cloud`.`configuration` (category, instance, component, name, value, description, default_value) VALUES ('Advanced', 'DEFAULT', 'management-server', 'vmware.snapshot.backup.session.timeout', '1200', 'VMware client timeout in seconds for snapshot backup', '1200');
