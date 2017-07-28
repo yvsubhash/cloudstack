05ad256f5968030c27800ce591039fda68419765
santosh <vedula_santosh@persistent.co.in>
2017-07-28 15:25:46 +0530

    CS-51871:Internal LB List Repeat

--- ui/scripts/vpc.js
+++ ui/scripts/vpc.js
@@ -745,8 +745,19 @@
                         }
                     },
                     dataProvider: function(args) {
+                        var array1 = [];
+                        if (args.filterBy != null) {
+                            if (args.filterBy.search != null && args.filterBy.search.by != null && args.filterBy.search.value != null) {
+                                switch (args.filterBy.search.by) {
+                                    case "name":
+                                        if (args.filterBy.search.value.length > 0)
+                                            array1.push("&keyword=" + args.filterBy.search.value);
+                                        break;
+                                }
+                            }
+                        }
                         $.ajax({
-                            url: createURL('listLoadBalancers'),
+                            url: createURL('listLoadBalancers&page=' + args.page + '&pagesize=' + pageSize + array1.join("")),
                             data: {
                                 networkid: args.context.networks[0].id,
                                 listAll: true
